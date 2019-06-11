5ee043f76414ab2bafc4e02ad368ab42a315ab27
Kunj Bihari <kunjbihari1756@gmail.com>
2019-06-11 13:45:28 +0530

    9. Sending Data via Http

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,38 +1,31 @@
 import React, { useState } from "react";
+import axios from "axios";
+
 const todo = props => {
-  // const [todoName, setTodoName] = useState('');
-  // const [todoList, setTodoList] = useState([]);
-  const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
-
-  const inputChangeHandler = event => {
-    setTodoState({
-      userInput: event.target.value,
-      todoList: todoState.todoList
-    });
-  };
-
+  const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const inputChangeHandler = (event)=> {
+    setTodoName(event.target.value);
+  }
   const todoAddHandler = () => {
-    setTodoState({
-      userInput: todoState.userInput,
-      todoList: todoState.todoList.concat(todoState.userInput)
-    });
-  };
+    // concat() will return a new array
+    setTodoList(todoList.concat(todoName));
+    axios.post('https://fir-6cd72.firebaseio.com/todo.json',{name:todoName})
+    .then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 
   return (
-     <React.Fragment>
-      <input
-        type="text"
-        placeholder="Todo"
-        onChange={inputChangeHandler}
-        value={todoState.userInput}
-      />
-      <button type="button" onClick={todoAddHandler}>
-        Add
-      </button>
+    <React.Fragment>
+      <input type="text" placeholder="Todo" 
+      onChange={inputChangeHandler} 
+      value={todoName}/>
+      <button type="button" onClick={todoAddHandler}>Add</button>
       <ul>
-        {todoState.todoList.map(todo => (
-          <li key={todo}>{todo}</li>
-        ))}
+      {todoList.map(todo=> <li>{todo}</li>)}
       </ul>
     </React.Fragment>
   );
