1a9d2458667058054bbed6bc1405c17379d21520
Kunj Bihari <kunjbihari1756@gmail.com>
2019-06-11 12:27:10 +0530

    7. Using One State Instead

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
 const todo = props => {
-  const [todoName, setTodoName] = useState('');
-  const [todoList, setTodoList] = useState([]);
-  const inputChangeHandler = (event)=> {
-    setTodoName(event.target.value);
-  }
+  // const [todoName, setTodoName] = useState('');
+  // const [todoList, setTodoList] = useState([]);
+  const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
+
+  const inputChangeHandler = event => {
+    setTodoState({
+      userInput: event.target.value,
+      todoList: todoState.todoList
+    });
+  };
+
   const todoAddHandler = () => {
-    // concat() will return a new array
-    setTodoList(todoList.concat(todoName))
-  }
+    setTodoState({
+      userInput: todoState.userInput,
+      todoList: todoState.todoList.concat(todoState.userInput)
+    });
+  };
 
   return (
-    <React.Fragment>
-      <input type="text" placeholder="Todo" 
-      onChange={inputChangeHandler} 
-      value={todoName}/>
-      <button type="button" onClick={todoAddHandler}>Add</button>
+     <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={todoState.userInput}
+      />
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
       <ul>
-      {todoList.map(todo=> <li key={todo}>{todo}</li>)}
+        {todoState.todoList.map(todo => (
+          <li key={todo}>{todo}</li>
+        ))}
       </ul>
     </React.Fragment>
   );
