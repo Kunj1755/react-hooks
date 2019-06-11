48a12a48507bbb13dae12a487e33a04a2ad1caa6
Kunj Bihari <kunjbihari1756@gmail.com>
2019-06-11 14:15:04 +0530

    10. The useEffect() Hook

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
+
+// Pass a function to useEffect()
+useEffect(() => {
+    axios.get('https://fir-6cd72.firebaseio.com/todo.json').then(result => {
+      // console.log(result);
+      const todoData = result.data;
+      const todos = [];
+      for (const key in todoData) {
+        todos.push({id: key, name: todoData[key].name})
+      }
+      setTodoList(todos);
+    });
+  });
+
   const inputChangeHandler = (event)=> {
     setTodoName(event.target.value);
   }
@@ -25,7 +39,7 @@ const todo = props => {
       value={todoName}/>
       <button type="button" onClick={todoAddHandler}>Add</button>
       <ul>
-      {todoList.map(todo=> <li>{todo}</li>)}
+      {todoList.map(todo=> <li key={todo.id}>{todo.name}</li>)}
       </ul>
     </React.Fragment>
   );
