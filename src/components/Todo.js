8fdee201718c29cf570ff90624d20b5b17228400
Kunj Bihari <kunjbihari1756@gmail.com>
2019-06-26 21:13:46 +0530

    15. State and Effects Gotchas

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,62 +2,79 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Todo = props => {
-  const [todoName, setTodoName] = useState('');
+  const [todoName, setTodoName] = useState("");
+  const [submittedTodo, setSubmittedTodo] = useState(null);
   const [todoList, setTodoList] = useState([]);
 
-// Pass a function to useEffect() as its 1st argument
-useEffect(() => {
-    axios.get('https://fir-6cd72.firebaseio.com/todo.json').then(result => {
+  // Pass a function to useEffect() as its 1st argument
+  useEffect(() => {
+    axios.get("https://fir-6cd72.firebaseio.com/todo.json").then(result => {
       // console.log(result);
       const todoData = result.data;
       const todos = [];
       for (const key in todoData) {
-        todos.push({id: key, name: todoData[key].name})
+        todos.push({ id: key, name: todoData[key].name });
       }
       setTodoList(todos);
     });
-    return ()=>{
+    return () => {
       console.log("cleanup");
-    }
-  },[todoName]);
+    };
+  }, [todoName]);
 
   const mouseMoveHandler = event => {
-      console.log(event.clientX, event.clientY);
-  }
+    console.log(event.clientX, event.clientY);
+  };
 
-  useEffect(()=>{
-    document.addEventListener('mousemove', mouseMoveHandler );
+  useEffect(() => {
+    document.addEventListener("mousemove", mouseMoveHandler);
     // return is used to write clean up code
-    return(()=>{
+    return () => {
       // Remove the mousemove listener that executes mouseMoveHandler
-      document.removeEventListener('mousemove', mouseMoveHandler);
-    });
-  },[]);
+      document.removeEventListener("mousemove", mouseMoveHandler);
+    };
+  }, []);
 
-  const inputChangeHandler = (event)=> {
+  useEffect(() => {
+    if (submittedTodo) {
+      setTodoList(todoList.concat(submittedTodo));
+    }
+  }, [submittedTodo]);
+
+  const inputChangeHandler = event => {
     setTodoName(event.target.value);
-  }
+  };
   const todoAddHandler = () => {
-    // concat() will return a new array
-    setTodoList(todoList.concat(todoName));
-    axios.post('https://fir-6cd72.firebaseio.com/todo.json',{name:todoName})
-    .then(res => {
-      console.log(res);
-    }).catch(err => {
-      console.log(err);
-    })
-  }
+    axios
+      .post("https://fir-6cd72.firebaseio.com/todo.json", { name: todoName })
+      .then(res => {
+        setTimeout(() => {
+          const todoItem = { id: res.data.name, name: todoName };
+          setSubmittedTodo(todoItem);
+        }, 3000);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   return (
     <React.Fragment>
-      <input type="text" placeholder="Todo" 
-      onChange={inputChangeHandler} 
-      value={todoName}/>
-      <button type="button" onClick={todoAddHandler}>Add</button>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={todoName}
+      />
+      <button type="button" onClick={todoAddHandler}>
+        Add
+      </button>
       <ul>
-      {todoList.map(todo=> <li key={todo.id}>{todo.name}</li>)}
+        {todoList.map(todo => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
       </ul>
     </React.Fragment>
-);
-}
+  );
+};
 export default Todo;
