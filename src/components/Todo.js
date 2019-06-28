d5f0ab0f11f18e0e29debeb9098ac59e271bc2cf
Kunj Bihari <kunjbihari1756@gmail.com>
2019-06-28 15:50:28 +0530

    19. Preparing & Optimizing

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,9 @@
-import React, { useEffect, useReducer, useRef } from "react";
+import React, { useState, useEffect, useReducer, useRef } from "react";
 import axios from "axios";
+import List from "./List";
 
 const Todo = props => {
+  const [inputIsValid, setInputIsValid] = useState(false);
   //const [todoName, setTodoName] = useState("");
   //const [submittedTodo, setSubmittedTodo] = useState(null);
   // As we are using useReducer. Comment this line of code
@@ -58,14 +60,22 @@ const Todo = props => {
     console.log(event.clientX, event.clientY);
   };
 
-  useEffect(() => {
-    document.addEventListener("mousemove", mouseMoveHandler);
-    // return is used to write clean up code
-    return () => {
-      // Remove the mousemove listener that executes mouseMoveHandler
-      document.removeEventListener("mousemove", mouseMoveHandler);
-    };
-  }, []);
+  const inputValidationHandler = event => {
+    if (event.target.value.trim() === "") {
+      setInputIsValid(false);
+    } else {
+      setInputIsValid(true);
+    }
+  };
+
+  // useEffect(() => {
+  //   document.addEventListener("mousemove", mouseMoveHandler);
+  //   // return is used to write clean up code
+  //   return () => {
+  //     // Remove the mousemove listener that executes mouseMoveHandler
+  //     document.removeEventListener("mousemove", mouseMoveHandler);
+  //   };
+  // }, []);
 
   // useEffect(() => {
   //   if (submittedTodo) {
@@ -112,17 +122,20 @@ const Todo = props => {
         // onChange={inputChangeHandler}
         // value={todoName}
         ref={todoInputRef}
+        onChange={inputValidationHandler}
+        style={{ backgroundColor: inputIsValid ? "transparent" : "red" }}
       />
       <button type="button" onClick={todoAddHandler}>
         Add
       </button>
-      <ul>
+      {/* <ul>
         {todoList.map(todo => (
           <li key={todo.id} onClick={todoRemoveHandler.bind(this, todo.id)}>
             {todo.name}
           </li>
         ))}
-      </ul>
+      </ul> */}
+      <List items={todoList} onClick={todoRemoveHandler} />
     </React.Fragment>
   );
 };
